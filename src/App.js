5e9f48b30b0f25acd41b1015fa599829792f49f6
DavidPerezGarcia <david.perez@sigob.com.mx>
2024-01-27 12:16:52 -0700

    added colors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import styled from 'styled-components';
-import React, {useState} from 'react';
+import styled, { keyframes } from 'styled-components';
 
+import React, {useState} from 'react';
+import colors from './styles/colors';
 
 function App() {
   const [clickedSquares, setClickedSquares] = useState([]);
@@ -20,14 +21,30 @@ function App() {
   return (
     <MainDiv>
       <Board>
-        {Array.from({ length: 200 }).map((_, index) => (
+        {Array.from({ length: 16 }).map((_, index) => (
           <Square key={index} isclicked={clickedSquares.includes(index)} 
-            onClick={() => handleSquareClick(index)}>{index}</Square>
+            onClick={() => handleSquareClick(index)}></Square>
         ))}
       </Board>
     </MainDiv>
   );
 }
+
+const rippleAnimation = keyframes`
+  0% {
+    background-color: ${colors.green};
+    transform: scale(1);
+  }
+  50% {
+    background-color: ${colors.white}; /* Cambia a otro color al medio de la animación */
+    transform: scale(0.9);
+  }
+  100% {
+    background-color: ${colors.green};
+    transform: scale(1);
+  }
+`;
+
 
 const MainDiv = styled.div`
   background-color: #FFFCF7;
@@ -39,29 +56,35 @@ const MainDiv = styled.div`
 `;
 
 const Board = styled.div`
-  background-color: #D1C952;
+  background-color: ${colors.yellow};
   border-radius: 10px;
   padding: 10px;
   border-width: 10px;
   height: 78vh;
-  width: 90vw;
+  width: 78vh;
   display: grid;
-  grid-template-columns: repeat(20, 1fr);
-  grid-template-rows: repeat(10, 1fr); /* Added for rows */
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(4, 1fr); /* Added for rows */
   grid-gap: 5px; /* Added colon */
   
   @media (max-width: 768px) {
     width: 95vw;
   }
 `;
-
 const Square = styled.div`
-  background-color: ${({ isclicked }) => (isclicked ? 'green' : 'white')};
+  background-color: ${({ isclicked }) => (isclicked ? colors.green : 'white')};
   border-radius: 5px;
   height: 100%;
   cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
+  transition: background-color 0.2s ease; /* Agrega una transición suave */
+  
+  &:active {
+    animation: ${rippleAnimation} 0.3s ease;
+  }
 `;
+
+
 export default App;
---
+++ src/styles/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  green: '#6CEFAD',
+  blue: '#6C6CEF',
+  red: '#EF6CAE',
+  yellow: '#EFEF6C',
+  background: '#fff',
+};
+
+export default colors;
